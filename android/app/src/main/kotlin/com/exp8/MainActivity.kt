e3b991a932dc701c543c991c14859cbf8bffac4b
Paulo <pvba02@gmail.com>
2019-10-10 11:10:10 -0600

    Starting the project

---
+++ android/app/src/main/kotlin/com/exp8/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.exp8
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
