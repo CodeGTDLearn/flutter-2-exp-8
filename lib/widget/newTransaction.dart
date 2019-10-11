e0fa3483886f0d4b1c6733a2b61cf553ab5c5a86
Paulo <pvba02@gmail.com>
2019-10-11 11:32:37 -0600

    newTransaction card: title + amount + date structure + addbutton

--- lib/widget/newTransaction.dart
+++ lib/widget/newTransaction.dart
@@ -18,3 +18,50 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
+    return Card(
+      elevation: 8,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Title'),
+              controller: titleController,
+              onSubmitted: (_) => _submitData(),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Amount'),
+              onSubmitted: (_) => _submitData(),
+              controller: amountController,
+              keyboardType: TextInputType.number,
+            ),
+            Container(
+              height: 70,
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text(_selectedDate == null
+                        ? 'No date'
+                        : 'Date: ${DateFormat.yMMMd().format(_selectedDate)}'),
+                  ),
+                  FlatButton(
+                    child:
+                        Text('Date', style: Theme.of(context).textTheme.button),
+                    onPressed: _showDataPicker,
+                  )
+                ],
+              ),
+            ),
+            RaisedButton(
+              onPressed: _submitData,
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).textTheme.button.color,
+              child: Text('Add Transaction'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
