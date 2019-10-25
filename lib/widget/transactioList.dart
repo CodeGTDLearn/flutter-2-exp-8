3ac8006ed0e24086964c22702f24999d0851f20e
PauloPortfolio <pvba02@gmail.com>
2019-10-25 12:53:36 -0400

    Transaction List Refact with mediaqueries

--- lib/widget/transactioList.dart
+++ lib/widget/transactioList.dart
@@ -1,6 +1,5 @@
 import 'package:exp8/entity/transaction.dart';
 import 'package:flutter/material.dart';
-
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
@@ -11,53 +10,48 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 450,
-      child: _listTransaction.isEmpty
-          ? Column(
-              children: <Widget>[
-                Text('No items yet'),
-                SizedBox(height: 10),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    'assets/images/waiting.png',
-                    fit: BoxFit.cover,
-                  ),
-                )
-              ],
-            )
-          : ListView.builder(
-              itemCount: _listTransaction.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 8,
-                  margin: EdgeInsets.all(5),
-                  child: ListTile(
-                      leading: CircleAvatar(
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: FittedBox(
-                              child:  
-                                  Text("\$${_listTransaction[index].amount}")),
-                        ),
+    return _listTransaction.isEmpty
+        ? Column(
+            children: <Widget>[
+              Text('No items yet'),
+              SizedBox(height: 10),
+              Container(
+                height: 200,
+                child: Image.asset(
+                  'assets/images/waiting.png',
+                  fit: BoxFit.cover,
+                ),
+              )
+            ],
+          )
+        : ListView.builder(
+            itemCount: _listTransaction.length,
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 8,
+                margin: EdgeInsets.all(5),
+                child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FittedBox(
+                            child: Text("\$${_listTransaction[index].amount}")),
                       ),
-                      title: Text(
-                        _listTransaction[index].title,
-                        style: Theme.of(context).textTheme.title,
-                      ),
-                      subtitle: Text(DateFormat.yMMMd()
-                          .format(_listTransaction[index].date)),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        color: Theme.of(context).errorColor,
-                        onPressed: () =>
-                            deleteTxDAO(_listTransaction[index].id),
-                      )),
-                );
-              },
-            ),
-    );
+                    ),
+                    title: Text(
+                      _listTransaction[index].title,
+                      style: Theme.of(context).textTheme.title,
+                    ),
+                    subtitle: Text(DateFormat.yMMMd()
+                        .format(_listTransaction[index].date)),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => deleteTxDAO(_listTransaction[index].id),
+                    )),
+              );
+            },
+          );
   }
 }
